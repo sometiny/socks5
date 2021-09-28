af8ef800948475638a130a87a6a2f10b95bb4ca6
anlige <zhanghuiguoanlige@126.com>
2021-09-28 09:27:33 +0800

    PacLoader;

--- Socks5/Pac.cs
+++ Socks5/Pac.cs
@@ -32,72 +32,28 @@ namespace IocpSharp.Socks5
         public static void Process(Stream stream, string hostListFile, string serveAt)
         {
             string firstLine = ReadHttpHeader(stream);
-            byte[] body;
-            byte[] header;
             if (!firstLine.StartsWith("GET /pac"))
             {
-                body = Encoding.UTF8.GetBytes("<h4>404 Not Found</h4>");
-                header = Encoding.UTF8.GetBytes($"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\nServer: PacServer/1.0\r\nContent-Length:{body.Length}\r\nConnection: close\r\n\r\n");
-
-                stream.Write(header, 0, header.Length);
-                stream.Write(body, 0, body.Length);
-                stream.Close();
+                SendResponse(stream, 404, "Not Found", "<h4>404 Not Found</h4>");
                 return;
             }
-            string pac = GetPac(hostListFile, serveAt);
+            string pac = PacLoader.GetPac(hostListFile, serveAt);
 
-            body = Encoding.UTF8.GetBytes(pac);
-            header = Encoding.UTF8.GetBytes($"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nServer: PacServer/1.0\r\nContent-Length:{body.Length}\r\nConnection: close\r\n\r\n");
+            SendResponse(stream, 200, "OK", pac, "text/plain");
+        }
+
+        private static void SendResponse(Stream stream, int statusCode, string statusText, string body, string contentType = "text/html")
+        {
+            SendResponse(stream, statusCode, statusText, Encoding.UTF8.GetBytes(body), contentType);
+        }
+
+        private static void SendResponse(Stream stream, int statusCode, string statusText, byte[] body, string contentType = "text/html")
+        {
+            byte[] header = Encoding.UTF8.GetBytes($"HTTP/1.1 {statusCode} {statusText}\r\nContent-Type: {contentType}\r\nServer: PacServer/1.0\r\nContent-Length:{body.Length}\r\nConnection: close\r\n\r\n");
 
             stream.Write(header, 0, header.Length);
             stream.Write(body, 0, body.Length);
             stream.Close();
-        }
-
-        public static string Script = $@"var proxy = 'SOCKS5 {{SERVE_AT}};';
-var direct = 'DIRECT;';
-var hasOwnProperty = Object.hasOwnProperty;
-
-function FindProxyForURL(url, host) {{
-    var suffix;
-    var pos = host.lastIndexOf('.');
-    pos = host.lastIndexOf('.', pos - 1);
-    while(1) {{
-        if (pos <= 0) {{
-            if (hasOwnProperty.call(domains, host)) {{
-                return proxy;
-            }} else {{
-                return direct;
-            }}
-        }}
-        suffix = host.substring(pos + 1);
-        if (hasOwnProperty.call(domains, suffix)) {{
-            return proxy;
-        }}
-        pos = host.lastIndexOf('.', pos - 1);
-    }}
-}}";
-        private static string _pacList = null;
-
-        public static void Reset()
-        {
-            _pacList = null;
-        }
-
-        public static string GetPac(string file, string serveAt)
-        {
-            if (_pacList != null)
-            {
-                return _pacList;
-            }
-            var lines = File.ReadAllLines(file);
-            lines = lines.Where(t => !string.IsNullOrEmpty(t)).Distinct().Select(t => "  \"" + t.ToLower() + "\" : 1").ToArray();
-            StringBuilder sb = new StringBuilder();
-            sb.Append("var domains = {\r\n");
-            sb.Append(string.Join(", \r\n", lines));
-            sb.Append("\r\n};\r\n");
-            sb.Append(Script.Replace("{SERVE_AT}", serveAt));
-            return _pacList = sb.ToString();
         }
     }
 }
---
+++ Socks5/PacLoader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace IocpSharp.Socks5
+{
+    internal class PacLoader
+    {
+        public static string Script = $@"var proxy = 'SOCKS5 {{SERVE_AT}};';
+var direct = 'DIRECT;';
+var hasOwnProperty = Object.hasOwnProperty;
+
+function FindProxyForURL(url, host) {{
+    var suffix;
+    var pos = host.lastIndexOf('.');
+    pos = host.lastIndexOf('.', pos - 1);
+    while(1) {{
+        if (pos <= 0) {{
+            if (hasOwnProperty.call(domains, host)) {{
+                return proxy;
+            }} else {{
+                return direct;
+            }}
+        }}
+        suffix = host.substring(pos + 1);
+        if (hasOwnProperty.call(domains, suffix)) {{
+            return proxy;
+        }}
+        pos = host.lastIndexOf('.', pos - 1);
+    }}
+}}";
+        private static string _pacList = null;
+
+        public static void Reset()
+        {
+            _pacList = null;
+        }
+
+        public static string GetPac(string file, string serveAt)
+        {
+            if (_pacList != null)
+            {
+                return _pacList;
+            }
+            var lines = File.ReadAllLines(file);
+            lines = lines.Where(t => !string.IsNullOrEmpty(t)).Distinct().Select(t => "  \"" + t.ToLower() + "\" : 1").ToArray();
+            StringBuilder sb = new StringBuilder();
+            sb.Append("var domains = {\r\n");
+            sb.Append(string.Join(", \r\n", lines));
+            sb.Append("\r\n};\r\n");
+            sb.Append(Script.Replace("{SERVE_AT}", serveAt));
+            return _pacList = sb.ToString();
+        }
+    }
+}
