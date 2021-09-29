478be33a4aaab79ad1b6a766078795157d4e1144
anlige <zhanghuiguoanlige@126.com>
2021-09-29 14:20:51 +0800

    Utils独立；

--- src/Commands/ConnectCommand.cs
+++ src/Commands/ConnectCommand.cs
@@ -18,7 +18,7 @@ namespace IocpSharp.Socks5.Commands
 
             _clientStream = requestStream;
             //连接远程服务器
-            Stream remoteStream = ConnectRemote(request.RemoteEndPoint, out IPEndPoint connectedEndPoint);
+            Stream remoteStream = Utils.ConnectRemote(request.RemoteEndPoint, out IPEndPoint connectedEndPoint);
 
 
             //连接成功后，发送响应数据到客户端
@@ -31,13 +31,13 @@ namespace IocpSharp.Socks5.Commands
             //开始对拷，不需要缓冲区了，多此一举
             if (_clientStream is BufferedNetworkStream buffered) buffered.Buffered = false;
 
-            _clientStream.CopyToAsync(_remoteStream).ContinueWith(copyFinished);
-            _remoteStream.CopyToAsync(_clientStream).ContinueWith(copyFinished);
+            _clientStream.CopyToAsync(_remoteStream).ContinueWith(CopyFinished);
+            _remoteStream.CopyToAsync(_clientStream).ContinueWith(CopyFinished);
         }
 
         private int _completed = 0;
 
-        private void copyFinished(Task task)
+        private void CopyFinished(Task task)
         {
             if (System.Threading.Interlocked.Increment(ref _completed) == 2)
             {
@@ -81,47 +81,6 @@ namespace IocpSharp.Socks5.Commands
             response[responseLength - 2] = (byte)((endpoint.Port >> 8) & 0xff);
 
             _clientStream.Write(response, 0, responseLength);
-        }
-
-        /// <summary>
-        /// 连接远程服务器
-        /// </summary>
-        /// <param name="endpoint">待连接的远程结点</param>
-        /// <param name="connectedEndPoint">连接成功的远程结点</param>
-        /// <returns></returns>
-        public static Stream ConnectRemote(EndPoint endpoint, out IPEndPoint connectedEndPoint)
-        {
-            if (endpoint is DnsEndPoint dnsEndPoint)
-            {
-                //解析主机
-                endpoint = new IPEndPoint(ResolveDnsHost(dnsEndPoint.Host), dnsEndPoint.Port);
-            }
-
-            Socket remoteSocket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            remoteSocket.NoDelay = true;
-            remoteSocket.Connect(endpoint);
-
-            connectedEndPoint = endpoint as IPEndPoint;
-
-            return new NetworkStream(remoteSocket, true);
-        }
-
-        /// <summary>
-        /// 解析主机
-        /// </summary>
-        /// <param name="host">主机名</param>
-        /// <param name="ipV6Prior">是否优先使用IPv6地址</param>
-        /// <returns></returns>
-        public static IPAddress ResolveDnsHost(string host, bool ipV6Prior = false)
-        {
-            IPAddress[] address = Dns.GetHostAddresses(host);
-            if (address == null || address.Length == 0) throw new SocketException(11001); //SocketError.HostNotFound
-
-            if (address.Length == 1) return address[0];
-
-            if (ipV6Prior) return address.OrderByDescending(t => t.AddressFamily).First();
-
-            return address.OrderBy(t => t.AddressFamily).First();
         }
     }
 }
---
+++ src/Utils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace IocpSharp.Socks5
+{
+    public class Utils
+    {
+
+        /// <summary>
+        /// 连接远程服务器
+        /// </summary>
+        /// <param name="endpoint">待连接的远程结点</param>
+        /// <param name="connectedEndPoint">连接成功的远程结点</param>
+        /// <returns></returns>
+        public static Stream ConnectRemote(EndPoint endpoint, out IPEndPoint connectedEndPoint)
+        {
+            if (endpoint is DnsEndPoint dnsEndPoint)
+            {
+                //解析主机
+                endpoint = new IPEndPoint(ResolveDnsHost(dnsEndPoint.Host), dnsEndPoint.Port);
+            }
+
+            Socket remoteSocket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            remoteSocket.NoDelay = true;
+            remoteSocket.Connect(endpoint);
+
+            connectedEndPoint = endpoint as IPEndPoint;
+
+            return new NetworkStream(remoteSocket, true);
+        }
+
+        /// <summary>
+        /// 解析主机
+        /// </summary>
+        /// <param name="host">主机名</param>
+        /// <param name="ipV6Prior">是否优先使用IPv6地址</param>
+        /// <returns></returns>
+        public static IPAddress ResolveDnsHost(string host, bool ipV6Prior = false)
+        {
+            IPAddress[] address = Dns.GetHostAddresses(host);
+            if (address == null || address.Length == 0) throw new SocketException(11001); //SocketError.HostNotFound
+
+            if (address.Length == 1) return address[0];
+
+            if (ipV6Prior) return address.OrderByDescending(t => t.AddressFamily).First();
+
+            return address.OrderBy(t => t.AddressFamily).First();
+        }
+    }
+}
