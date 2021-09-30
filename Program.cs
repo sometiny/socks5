a036dc2604853cfb94b69c9622d042de2a9c3b3d
anlige <zhanghuiguoanlige@126.com>
2021-09-30 09:44:34 +0800

    nothing；

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IocpSharp.Socks5
 {
@@ -16,10 +12,10 @@ namespace IocpSharp.Socks5
             {
                 server.Start("0.0.0.0", 4088);
                 Console.WriteLine("服务器启动成功，监听地址：" + server.LocalEndPoint.ToString());
-            }
-            catch (Exception e)
+            } 
+            catch (Exception ex)
             {
-                Console.WriteLine(e.ToString());
+                Console.WriteLine(ex.ToString());
             }
 
             Console.ReadLine();
